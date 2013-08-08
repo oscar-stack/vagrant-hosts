e77dbcbd149ec2c9bf420ebddbea654a0e91712b
Adrien Thebo <git@somethingsinistral.net>
2013-08-08 00:19:49 -0700

    Correct yardoc layout

--- lib/vagrant-hosts/provisioner/hostname.rb
+++ lib/vagrant-hosts/provisioner/hostname.rb
@@ -1,11 +1,12 @@
 require 'vagrant'
+
+# Abstract the details of setting a guest hostname on different versions of
+# Vagrant.
+#
+# Vagrant commit 61d2f9f96fc0f0ef5869c732674f25c4ccc85c8c converts the
+# #change_host_name # method to a capability, which breaks the API between
+# 1.1 and 1.2. :(
 module VagrantHosts::Provisioner::Hostname
-  # Abstract the details of setting a guest hostname on different versions of
-  # Vagrant.
-  #
-  # Vagrant commit 61d2f9f96fc0f0ef5869c732674f25c4ccc85c8c converts the
-  # #change_host_name # method to a capability, which breaks the API between
-  # 1.1 and 1.2. :(
 
   # @param name [String] The new hostname to apply on the guest
   def change_host_name(name)
