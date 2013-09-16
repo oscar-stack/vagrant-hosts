c79a079d3dc8fefdbc283f1fa3424a36798112a5
Reid Vandewiele <reid@puppetlabs.com>
2013-09-16 12:10:21 -0700

    Add/move i18n definition
    In a previous commit an error message was changed to call a different
    i18n key, but the key was not moved. This commit rectifies that change
    omission.
    
    This commit also removes the unused SyncHostsFailed error class.

--- lib/vagrant-hosts/cap.rb
+++ lib/vagrant-hosts/cap.rb
@@ -2,10 +2,6 @@ module VagrantHosts
   module Cap
     module SyncHosts
 
-      class SyncHostsFailed < Vagrant::Errors::VagrantError
-        error_key(:sync_hosts_failed, 'vagrant_hosts.cap.sync_hosts')
-      end
-
       class UnknownVersion < Vagrant::Errors::VagrantError
         error_key(:unknown_version, 'vagrant_hosts.cap.sync_hosts')
       end
