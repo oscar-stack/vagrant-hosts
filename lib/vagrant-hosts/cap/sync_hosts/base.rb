b199529cbb0c1aec929d908e2b8bb00f1126ef8b
Reid Vandewiele <reid@puppetlabs.com>
2014-03-21 12:49:33 -0700

    Add support for Vagrant 1.5.x
    The vagrant-hosts plugin is very opinionaed about what versions of
    Vagrant it will run under. This patch makes sure that it's opinion of
    Vagrant 1.5.x is a positive one.

--- lib/vagrant-hosts/cap/sync_hosts/base.rb
+++ lib/vagrant-hosts/cap/sync_hosts/base.rb
@@ -28,7 +28,7 @@ class VagrantHosts::Cap::SyncHosts::Base
     case Vagrant::VERSION
     when /^1\.1/
       @machine.guest.change_host_name(name)
-    when /^1\.[234]/
+    when /^1\.[2-5]/
       @machine.guest.capability(:change_host_name, name)
     else
       raise VagrantHosts::Cap::SyncHosts::UnknownVersion, :vagrant_version => Vagrant::VERSION
