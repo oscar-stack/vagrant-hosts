8bd490791bb82f32b023e929bf368d4584eec892
Charlie Sharpsteen <source@sharpsteen.net>
2016-03-26 19:53:18 -0700

    Use a PTY when syncing /etc/hots on POSIX
    Prior to this patch, the hosts provisioner would fail when syncing /etc/hosts
    on POSIX systems that had requiretty set. This setting is a fairly common
    default for RedHat and Debian VM templates. Since the command run during sync
    doesn't generate any terminal output, allocating a PTY is relatively
    non-intrusive.
    
    Fixes oscar-stack/vagrant-hosts#59.

--- lib/vagrant-hosts/cap/sync_hosts/posix.rb
+++ lib/vagrant-hosts/cap/sync_hosts/posix.rb
@@ -15,7 +15,18 @@ class VagrantHosts::Cap::SyncHosts::POSIX < VagrantHosts::Cap::SyncHosts::Base
 
   def update_hosts
     upload_tmphosts
+
+    # Switch to PTY mode as this provider may execute across multiple machines
+    # which may not have requiretty set to false (i.e. because they're still
+    # booting and scripts that disable requiretty haven't run yet). Not doing
+    # this can have nasty side effects --- such as preventing machines from
+    # being destroyed.
+    old_pty_setting = @machine.config.ssh.pty
+    @machine.config.ssh.pty = true
+
     @machine.communicate.sudo('cat /tmp/hosts > /etc/hosts')
+  ensure
+    @machine.config.ssh.pty = old_pty_setting
   end
 
   # Generates content appropriate for a linux hosts file
