4af9b3e36ebfa7629a5937697d695a3727786ff0
Charlie Sharpsteen <source@sharpsteen.net>
2016-03-30 21:34:29 -0700

    Remove unused UnknownVersion error
    The logic which raised this error class was removed back in 2.3.0 with commit
    c78c1d6.

--- lib/vagrant-hosts/cap.rb
+++ lib/vagrant-hosts/cap.rb
@@ -1,15 +1,9 @@
 module VagrantHosts
   module Cap
     module SyncHosts
-
-      class UnknownVersion < Vagrant::Errors::VagrantError
-        error_key(:unknown_version, 'vagrant_hosts.cap.sync_hosts')
-      end
-
       require 'vagrant-hosts/cap/sync_hosts/base'
       require 'vagrant-hosts/cap/sync_hosts/posix'
       require 'vagrant-hosts/cap/sync_hosts/windows'
-
     end
   end
 end
