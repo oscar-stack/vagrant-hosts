598acc8c758c6f1757c2230fd908957011d51fe2
Charlie Sharpsteen <chuck@puppet.com>
2018-05-01 17:37:15 -0700

    Disable plugin initialization during tests
    This commit updates the test helper to set the VAGRANT_DISABLE_PLUGIN_INIT
    envirionment variable in addition to the VAGRANT_NO_PLUGINS variable
    during test suite execution. This allows Vagrant versions newer than 1.9
    to be tested alongside a production installation without plugin errors
    occurring.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@ $LOAD_PATH.unshift File.expand_path('../lib', __FILE__)
 # Disable Vagrant autoloading so that other plugins defined in the Gemfile for
 # Acceptance tests are not loaded.
 ENV['VAGRANT_NO_PLUGINS'] = '1'
+ENV['VAGRANT_DISABLE_PLUGIN_INIT'] = '1'
 
 # Load the vagrant-hosts plugin so that Provisioners and such can be found.
 require 'vagrant-hosts'
